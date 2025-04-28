6704818fcd5c72d98f03efda77ff1c6cd6dd4e03
Alghifarimaulaabi <Alghifarimaulaabi@gmail.com>
2025-04-28 23:37:48 +0700

    projectt

---
+++ script.js
@@ -0,0 +1,82 @@
+//menu
+var tombolMenu = $(".tombol-menu");
+var menu = $("nav .menu ul");
+
+function klikMenu() {
+    tombolMenu.click(function() {
+        menu.toggle(); 
+    });
+    menu.click(function() {
+        menu.toggle(); 
+    });
+}
+
+$(document).ready(function () {
+    var width = $(window).width();
+    if (width < 990) { 
+        klikMenu();
+    }
+});
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-250px";
+    } else {
+        sidebar.style.left = "0px";
+    }
+}
+
+
+//check lebar
+$(window).resize(function(){
+    var width = $(window).width();
+    if (width > 989) {
+        menu.css("display","block");
+        //display block
+    }else{
+        menu.css("display","none");
+    }
+    klikMenu();
+});
+
+function toggleFAQ() {
+    var faq = document.getElementById("faq");
+    faq.style.display = faq.style.display === "none" || faq.style.display === "" ? "block" : "none";
+}
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+    
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+    
+})
+
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000); // Ganti slide setiap 3 detik
+showSlide(currentIndex);
